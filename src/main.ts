33724753a314c8f7a9f1df26c73c9a9a764b1d78
LucasPontees <eulpontes@gmail.com>
2025-01-14 15:38:12 -0400

    fix error base_url index

--- src/main.ts
+++ src/main.ts
@@ -12,3 +12,11 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+// Adiciona conteúdo HTML após a montagem da aplicação
+document.body.innerHTML += `
+  <div class="content-wrapper">
+    <h1>Meu Conteúdo Adicional</h1>
+    <p>Este é um conteúdo adicionado via main.ts</p>
+  </div>
+`
